749979f59be653d76364b4db0f21d21e3eba07d3
MR. RIKI <104522915+MR-RIKI@users.noreply.github.com>
2022-05-01 09:42:59 +0600

    Add files via upload

---
+++ green.py
@@ -0,0 +1,22 @@
+import os, platform;from time import sleep
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    print ('WELCOME 64 BIT USER..!')
+    sleep (2)
+    from green import main_apv
+    main_apv()
+
+elif bit == '32bit':
+    os.system('clear')
+    print('WELCOME 32 BIT USER..!')
+    sleep (2)
+    from green32 import main_apv
+    main_apv()
