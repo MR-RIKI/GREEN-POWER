9847e8da083c9794c1e0b59beaaff80447f9ead0
MR. RIKI <104522915+MR-RIKI@users.noreply.github.com>
2022-06-30 18:27:50 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+import os,platform
+
+riki=platform.architecture()[0]
+
+os.system('git pull')
+
+if riki=="32bit":
+    os.system('am start https://www.facebook.com/Itz.D3M09')
+    __import__("riki").rikiusername()
+
+elif riki=="64bit":
+    os.system('am start https://www.facebook.com/Itz.D3M09')
+    __import__("riki").rikiusername()
